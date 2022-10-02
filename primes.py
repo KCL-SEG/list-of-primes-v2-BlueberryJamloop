adacebd0ed10835d39d99622bc02c3b8b1ed4a63
Maja G <maja293@outlook.com>
2022-10-02 12:38:25 +0100

    attempt one

--- primes.py
+++ primes.py
@@ -1,6 +1,30 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def isPrime(number):
+    for x in range(2, number):
+        if number % x == 0:
+            return False
+    return True
+        
+        
+        
+
 def primes(number_of_primes):
-    list = []
-    return list
+    try:
+        listOfPrimes = []
+        if number_of_primes <= 0:
+            raise ValueError("Only positive numbers")
+
+        number = 2
+        
+        while len(listOfPrimes) < number_of_primes:
+            
+            if isPrime(number):
+                listOfPrimes.append(number)
+            number += 1
+        print(listOfPrimes)
+        return listOfPrimes
+    except ValueError:
+        print("Number of primes has to be positive")
+
